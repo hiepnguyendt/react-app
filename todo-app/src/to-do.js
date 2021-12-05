2ff278188b9ba8739389743043e3c8c1a5cbe86c
hiepnguyendt <89370886+hiepnguyendt@users.noreply.github.com>
2021-12-05 15:19:21 +0700

    update

---
+++ todo-app/src/to-do.js
@@ -0,0 +1,13 @@
+import react from "react";
+class Todo extends  react.Component{
+    constructor(pros){
+    super(pros)
+    }
+    render(){
+        return(
+            <div> Hi, Welcome to First Cloud Journey </div>
+        )
+
+    }
+}
+export default Todo
